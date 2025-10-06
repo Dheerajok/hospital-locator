8a00843d5675ba67572fb25e42d7a2ce60f67219
DHEERAJ OK <dheerajokfreefire@gmail.com>
2025-10-06 23:52:20 +0530

    erroe solved

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,18 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  // Load base Next.js + TypeScript config
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // 👇 Add your custom overrides here
+  {
+    rules: {
+      "@next/next/no-img-element": "off",      // allow <img> instead of <Image>
+      "react/no-unescaped-entities": "off",    // allow unescaped quotes in JSX
+      "no-unused-vars": "warn",                // downgrade unused var errors
+      "react/prop-types": "off",               // disable prop-types rule
+    },
+  },
 ];
 
 export default eslintConfig;
